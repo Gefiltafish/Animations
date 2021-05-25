a4cb78855d399b99b91d7ac570a6e3254f054075
Teodor Jonsson <teodor.jonsson@appliedtechnology.se>
2021-05-25 20:30:57 +0200

    fixed tyoe

--- ninja-profiles/src/components/pages/NinjaCardPage.tsx
+++ ninja-profiles/src/components/pages/NinjaCardPage.tsx
@@ -41,7 +41,7 @@ interface Ninja {
   imagePortraitUrl: string;
   imageWallOfLeetUrl: string;
   linkedIn: string;
-  mainText: '<p>I have been working in the software industry for over 10 years now, and even though the road has been bumpy, I don’t regret a single second of it! My experiences have been quite varied, from creating games to full blown web-based intranet platforms.</p><p>Because of this variation and these experiences I’ve found that I’ve acquired a thirst for front-end development and anything web related, which can range from mobile apps to web-based SaaS solutions. The fact that you can create ANYTHING with almost nothing is an exhilarating feeling. It’s one of the major reasons that keeps me going; the only limit is my imagination.</p><p>I absolutely love to travel and experience new cultures (and food!) when the opportunity presents itself. My goal in life is to visit as many places and experience as many different cultures as I possibly can. So far I can cross off Europe and half of Asia from my list, and I look forward to many more exciting adventures to come.</p>  ';
+  mainText: string;
   manager: string;
   name: string;
   office: string;
